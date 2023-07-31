3476677c9d1d64741c3e83bae67ce137cc8a363a
asimraza336 <69896110+asimraza336@users.noreply.github.com>
2023-07-31 22:40:36 +0500

    31/7/2023

--- accounts/financial_reports.py
+++ accounts/financial_reports.py
@@ -4,7 +4,9 @@ import json
 # API = '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'
 # API = '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'
 # API = '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'
-API = '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'
+# API = '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'
+API = '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'
+
 
 
 

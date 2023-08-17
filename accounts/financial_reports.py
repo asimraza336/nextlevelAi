bf5f33aab01f0b5fd0feb63c8c26f66a332dde00
Talha Sheikh <talhakhawar9292@gmail.com>
2023-08-17 20:12:40 +0500

    Update financial_reports.py

--- accounts/financial_reports.py
+++ accounts/financial_reports.py
@@ -8,37 +8,20 @@ import json
 # from config import *
 # API = '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'
 # API = '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'
-
-
-os.environ['OPENAI_API_KEY'] = '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'
+import tiktoken
+encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+os.environ['OPENAI_API_KEY'] = ''
 
 def generate_financial_insights(data):
-
-    try:
-        # sales_rep_name = data["sales_rep_name"]
-        sales_rep_company_name = data["sales_rep_company_name"]
-        # sales_rep_contact_number = data["sales_rep_contact_number"]
-        # sales_rep_email = data["sales_rep_email"]
-        #sales_rep_company_overview = data["sales_rep_company_overview"]
-        # sales_rep_company_website = data["sales_rep_company_website"]
-        sales_rep_company_industry = data["sales_rep_company_industry"]
-        #sales_rep_company_headquarters = data["sales_rep_company_headquarters"]
-        sales_rep_company_specialties = data["sales_rep_company_specialties"]
-
-        prospect_company_name = data["prospect_company_name"]
-        #prospect_company_overview = data["prospect_company_overview"]
-        prospect_company_industry = data["prospect_company_industry"]
-        #prospect_company_headquarters = data["prospect_company_headquarters"]
-        #prospect_company_size = data["prospect_company_size"]
-        #prospect_company_founded = data["prospect_company_founded"]
-        #prospect_company_specialties = data.get("prospect_company_specialties", None)
-        # data["prospect_company_specialties"]
-        focus_areas = data["focus_area"]
-        financial_raw_data = data["financial_raw_data"]
-    except Exception as e:
-        return e
-
-    chat_insights = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
+    sales_rep_company_name = data["sales_rep_company_name"]
+    sales_rep_company_industry = data["sales_rep_company_industry"]
+    sales_rep_company_specialties = data["sales_rep_company_specialties"]
+    prospect_company_name = data["prospect_company_name"]
+    prospect_company_industry = data["prospect_company_industry"]
+    #financial_raw_data = data.get("financial_raw_data", None)
+    financial_raw_data = data["financial_raw_data"]
+    focus_areas = "Show how T-Mobile for Business can offset debt and liabilities. Also show case how T-Mobile for Business can enhance Caresource using IOT, rural internet, drive membership experience, and drive rural Healthcare initiatives. How can T-Mobile for Business help Caresource from a medical devices perspective.How can T-Mobile for Business enhance EVV for Caresource?"
+    chat_insights = ChatOpenAI(temperature=0.7)
 
     # setting up the output parser
     insights_schema = ResponseSchema(name="insights",
@@ -50,11 +33,12 @@ def generate_financial_insights(data):
     response_schemas = [insights_schema]
     output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
     format_instructions = output_parser.get_format_instructions()
+    # setting up the prompt
     insights_template = """You are an AI sales representative assistant. Your task is to analyze the balance sheet of a prospect company and explain how your company can be beneficial for them. also include references and digital amounts in dollars from balance sheet.
-        {sales_rep_company_name} is an industry of {sales_rep_company_industry} with the following specialties {sales_rep_company_specialties}. There is a prospect company named {prospect_company_name} in the industry of {prospect_company_industry}. The balance sheet of {prospect_company_name} is given below. By analyzing the balance sheet, elaborate on how can {sales_rep_company_name} be beneficial for {prospect_company_name} in up to 250 words. And also make a statement on {focus_areas} and use digital amounts from balance sheet in answer as refrences.
-        balance sheet: ```{financial_raw_data}```
-        {format_instructions}
-        """
+{sales_rep_company_name} is an industry of {sales_rep_company_industry} with the following specialties {sales_rep_company_specialties}. There is a prospect company named {prospect_company_name} in the industry of {prospect_company_industry}. The balance sheet of {prospect_company_name} is given below. By analyzing the balance sheet, elaborate on how can {sales_rep_company_name} be beneficial for {prospect_company_name} in up to 250 words. And also make a statement on {focus_areas} and use digital amounts from balance sheet in answer as refrences.
+balance sheet: ```{financial_raw_data}```
+{format_instructions}
+"""
     prompt_template = ChatPromptTemplate.from_template(insights_template)
     customer_messages = prompt_template.format_messages(
                     sales_rep_company_name=sales_rep_company_name,
@@ -66,6 +50,7 @@ def generate_financial_insights(data):
                     financial_raw_data=financial_raw_data,
                     format_instructions=format_instructions)
     response = chat_insights(customer_messages)
+    print(response.content)
     try:
         output_dict = output_parser.parse(response.content)
         output_dict["message"] = "Success"
@@ -81,7 +66,8 @@ def generate_grants(data):
     prospect_company_name = data["prospect_company_name"]
     prospect_company_industry = data["prospect_company_industry"]
     grants_raw_data = data.get("grants_raw_data", None)
-    chat_insights = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
+
+
 
     # setting up the output parser
     grants_schema = ResponseSchema(name="grants",
@@ -107,9 +93,15 @@ raw data: ```{raw_data}```
                     prospect_company_industry=prospect_company_industry,
                     raw_data=grants_raw_data,
                     format_instructions=grants_format_instructions)
+    print(customer_messages[0].content)
+    num_tokens = len(encoding.encode(customer_messages[0].content))
+    if num_tokens >4000:
+        chat_insights = ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=0.7)
+    else:
+        chat_insights = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
     response = chat_insights(customer_messages)
     grants_output_dict = grants_output_parser.parse(response.content)
-	
+
     new_list = []
     try:
         for x in grants_output_dict["grants"]:
